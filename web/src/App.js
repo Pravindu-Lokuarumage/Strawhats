386777679e6889368cfb13382414fa0d3c5b5bfa
Pravindu-Lokuarumage <pravindu2000@gmail.com>
2020-09-01 14:19:56 +1000

    NEW: adding router

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import AboutUe from './pages/about-us';
+import Info from './pages/info';
+import Profile from './pages/profile';
+import NotFound from './pages/404';
+import Registration from './pages/registration';
+import Login from './pages/login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Bye update
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {  
+  render () {
+    return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component = {Profile}/>
+        <Route exact path="/info" component = {Info}/>
+        <Route exact path="/about-me" component = {AboutUe}/>
+        <Route exact path="/login" component = {Login}/>
+        <Route exact path="/registeration" component = {Registration}/>
+
+        <Route exact path ="/404" component = {NotFound}/>
+        <Redirect to="/404" />
+      </Switch>
+    </Router>
+    );
+  }
 }
 
 export default App;
