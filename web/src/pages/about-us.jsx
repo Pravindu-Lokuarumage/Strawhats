744abe2b98a81cc3c31f0618ab5cfe3750e4af5b
SmHamza <smhamza1312@gmail.com>
2020-09-04 01:03:32 +1000

    added a style.css file for further styling

--- web/src/pages/about-us.jsx
+++ web/src/pages/about-us.jsx
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import '../myStyle.css'
 import $ from "jquery";
-
-
 
 class  AboutUs extends Component {
     handleClick(){
@@ -14,17 +13,15 @@ class  AboutUs extends Component {
 		return(
             //page html
             //1.Welcome
-            //2.About us
-            //3.Our mission
-            <div className="container">
+            //2.Our Goal
+            //3.Our Team
+            //4.User Reviews
+            //5.Our contact info
+            <div class="container">
 				<div id="navbar"><Navbar></Navbar></div>
-                {/* <img src="https://t3.ftcdn.net/jpg/01/14/03/74/240_F_114037493_Ojbs6yZLQdJ48cqHmzRyAxlhIQUkGopV.jpg" class="img-fluid" alt="Cinque Terre"></img>
-                 <h1 class="text-center"> Welcome</h1>
-                 <h1>About us</h1> */} 
-
                  <div class="row">
-                    <div class="col-md-4">
-                        <h2 class="text-center">Welcome To HAP</h2>
+                    <div class="col-md-5 firstRow">
+                        <h2 class="text-center p-1 p-sm-3">Welcome To HAP</h2>
                         <h4 class="text-center"> Our Goal</h4>
                         <p class="text-left">Health and Prosperity (HAP).
                         Our website is based on the goal of provinding the users with an
@@ -34,15 +31,51 @@ class  AboutUs extends Component {
                         easy to use user interface and accessibility.
                         </p>
                     </div>
-                        <div class="col-md-6"><img src="https://t3.ftcdn.net/jpg/01/14/03/74/240_F_114037493_Ojbs6yZLQdJ48cqHmzRyAxlhIQUkGopV.jpg" class="float-right" width="350" height="270" alt="Cinque Terre"></img>
+                    <div class="col-md-6">
+                        <img src="https://t3.ftcdn.net/jpg/01/14/03/74/240_F_114037493_Ojbs6yZLQdJ48cqHmzRyAxlhIQUkGopV.jpg" class="float-right" width="350" height="270" alt="Cinque Terre"></img>
                     </div>
                 </div>
+
+                <div class="row">
+                    <div class="col-md-6 float-left">
+                            <img src="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX11814910.jpg" class="float-right" width="300" height="300" alt="Cinque Terre"></img>
+                    </div>
+                    <div class="col-md-6 float-right">
+                        <h2 class="text-center">Meet Our Team</h2>
+                        <h4 class="text-center"> Our Goal</h4>
+                        <p class="text-left">Health and Prosperity (HAP).
+                        Our website is based on the goal of provinding the users with an
+                        interferface through which they can keep track of their health goals
+                        and hopefully achieve them. Our sole purpose is to keep this website
+                        interesting enough for the users to keep coming back because of it's
+                        easy to use user interface and accessibility.
+                        </p>
+                    </div>
+                </div>    
+
+                <div class="row">
+                    <div class="col-md-5 firstRow">
+                        <h2 class="text-center p-1 p-sm-3">Welcome To HAP</h2>
+                        <h4 class="text-center"> Our Goal</h4>
+                        <p class="text-left">Health and Prosperity (HAP).
+                        Our website is based on the goal of provinding the users with an
+                        interferface through which they can keep track of their health goals
+                        and hopefully achieve them. Our sole purpose is to keep this website
+                        interesting enough for the users to keep coming back because of it's
+                        easy to use user interface and accessibility.
+                        </p>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="https://t3.ftcdn.net/jpg/01/14/03/74/240_F_114037493_Ojbs6yZLQdJ48cqHmzRyAxlhIQUkGopV.jpg" class="float-right" width="350" height="270" alt="Cinque Terre"></img>
+                    </div>
+                </div>
+
 
 				 <div className="form-group">
 					<label htmlFor="review">Review</label>
 					<input type="text" className="form-control" id="review" />
 				</div>
-				<button className="btn btn-success" onClick={this.handleClick}>Submit</button>
+				<button className="btn btn-primary" onClick={this.handleClick}>Submit</button>
 				<div id="footer"><Footer></Footer></div>
 			 </div>
     	);
