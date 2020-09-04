04be53387e630259fe0f653e0e16fb836dc774c5
SmHamza <smhamza1312@gmail.com>
2020-09-04 15:36:03 +1000

    for pulling

--- web/src/components/review.jsx
+++ web/src/components/review.jsx
@@ -7,16 +7,12 @@ import {Link} from 'react-router-dom';
 class Review extends Component { 
     constructor(props){
 		super(props)
-		/*this.state ={
-            like:''
-        }*/
-		//this.handleClick = this.handleClick.bind(this);
 	} 
   render(){
     return(
         <div className="container">
             <div className="text-center"><h3>Reviews</h3></div>
-            <div>{this.props.comment}</div>
+            <div>{}</div>
             <div></div>
         </div>
     );
