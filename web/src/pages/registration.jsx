3d38d94aeb21faf6ecc1903ce123fcaa3eeea347
SmHamza <smhamza1312@gmail.com>
2020-09-08 13:51:40 +1000

    formated the page

--- web/src/pages/registration.jsx
+++ web/src/pages/registration.jsx
@@ -54,16 +54,16 @@ class Registration extends Component {
     	        <div id="navbar"><Navbar></Navbar></div>
 
  			    <h1>Registration</h1>
- 			    <div class="form-group">
-			    	<label htmlfor="name">User</label>
+ 			    <div className="form-group">
+			    	<label htmlFor="name">User</label>
 			    	<input type="text" className="form-control" id="name" />
 			    </div>
-			    <div class="form-group">
-			    	<label htmlfor="password">Password</label>
+			    <div className="form-group">
+			    	<label htmlFor="password">Password</label>
 			    	<input type="password" className="form-control" id="password" />
 			    </div>
-			    <div class="form-group">
-			    	<label htmlfor="confirm">Confirm</label>
+			    <div className="form-group">
+			    	<label htmlFor="confirm">Confirm</label>
 			    	<input type="password" className="form-control" id="confirm_password" />
 			    </div>
 			    <button className="btn btn-success" id="register" onClick={this.handleClick}>Register</button>
