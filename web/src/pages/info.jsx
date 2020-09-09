bafe90e0abf386094a7fb676d0482b41806f47d0
Pravindu-Lokuarumage <pravindu2000@gmail.com>
2020-09-09 19:40:17 +1000

    NEW: heartrate and step data graphs

--- web/src/pages/info.jsx
+++ web/src/pages/info.jsx
@@ -1,18 +1,90 @@
 import React, { Component,useEffect } from 'react';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import Chart from '../components/chart';
 import $ from "jquery";
 
-const Info = () => {
-    return(
-        <div>
-                <div id="navbar"><Navbar></Navbar></div>
-                <div>
-                    here
+const API_URL = 'https://api-cyan-six.vercel.app/api';
+const currentUser = localStorage.getItem('user');
+
+class Info extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          hchartData:{},
+          schartData:{}
+        }
+    }
+    componentWillMount(){
+        this.getData();
+    }
+    componentDidUpdate(){
+        
+    }
+    getData(){
+        $.get(`${API_URL}/data/${currentUser}`)
+        .then(response => {
+            if (response[0]== null){
+            }
+            else{
+                var htime =[];
+                var hdata =[];
+                var stime =[];
+                var sdata =[];
+
+
+                response[0].heartrate.forEach(element => {
+                    htime.push(new Date(element.time).toTimeString().slice(0,8));
+                });
+                response[0].heartrate.forEach(element => {
+                    hdata.push(element.heartrate);
+                });
+                response[0].stepsperd.forEach(element => {
+                    stime.push(new Date(element.time).toTimeString().slice(0,8));
+                });
+                response[0].stepsperd.forEach(element => {
+                    sdata.push(element.stepsperd);
+                });
+                
+                this.setState({
+                    hchartData:{
+                      labels: htime,
+                      datasets:[
+                        {
+                          label:'Population',
+                          data:hdata,
+                          backgroundColor:'rgba(255, 99, 132, 0.6)'
+                        }
+                      ]
+                    },
+                    schartData:{
+                        labels: stime,
+                        datasets:[
+                          {
+                            label:'Population',
+                            data:sdata,
+                            backgroundColor:'rgba(255, 99, 132, 0.6)'
+                          }
+                        ]
+                      }
+                  });
+            }
+        })
+    }
+    render(){
+        return(
+            <div>
+                    <div id="navbar"><Navbar></Navbar></div>
+                    <div>
+                        <Chart chartData={this.state.hchartData}/>
+                        <Chart chartData={this.state.schartData}/>
+
+                    </div>
+                    <div id="footer"><Footer></Footer></div>
                 </div>
-			    <div id="footer"><Footer></Footer></div>
-            </div>
-    );
+        );
+    }
+    
 }
 
 export default Info;
