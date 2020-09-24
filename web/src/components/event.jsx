d0d1f6419101bdd196e72bb2e11afa5f3d6ae906
Pravindu-Lokuarumage <pravindu2000@gmail.com>
2020-09-25 01:53:20 +1000

    NEW:average heartrate

--- web/src/components/event.jsx
+++ web/src/components/event.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar'
+import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
-import Modal from 'react-bootstrap/Modal'
+import Row from 'react-bootstrap/Row'
 import Button from 'react-bootstrap/Button'
 import Chart from '../components/barChart'
 import {Link} from 'react-router-dom';
@@ -15,156 +16,81 @@ class Event extends Component {
         super(props)
         this.state = {
             test:"ok",
-            user_data:[],
-            steps:[],
-            distance:[],
-            calories:[],
-            show:false
+            user_data:[]
         }
-        
-		this.handleShow = this.handleShow.bind(this);
-		this.handleClose = this.handleClose.bind(this);
     } 
     static defaultProps = {
         start: new Date(),
         end: new Date(),
+        type:"jogging",
         users:[]
       }
     componentDidMount(){
-        this.props.users.map(user => {
-            console.log(user)
-            $.get(`${API_URL}/data/${user}`)
-            .then((response) => {
-                
-                var Tsteps = 0;  
-                var distance = 0;
-                var calories = 0;
-                response[0].stepsperd.forEach(element => {
-                    if( new Date(this.props.start).getTime() < new Date(element.time).getTime() && new Date(this.props.end).getTime() > new Date(element.time).getTime())
-                    {
-                        Tsteps = Tsteps + parseInt(element.stepsperd, 10)
-                    }
-                });   
-                distance = Tsteps/1.35;
-                calories = distance/18.8;                        
+        $.get(`${API_URL}/data/${this.props.user}`)
+        .then((response) => {
+            var Tsteps = 0; 
+            var count = 0;
+            var Trate = 0; 
+            var distance = 0;
+            var calories = 0;
+            response[0].stepsperd.forEach(element => {
+                if(new Date(this.props.start).getTime() < new Date(element.time).getTime() && new Date(this.props.end).getTime() > new Date(element.time).getTime())
+                {
+                    Tsteps = Tsteps + parseInt(element.stepsperd, 10)
+                }
+            }); 
+            response[0].heartrate.forEach(element => {
+                if(new Date(this.props.start).getTime() < new Date(element.time).getTime() && new Date(this.props.end).getTime() > new Date(element.time).getTime())
+                {
+                    Trate = Trate + parseInt(element.heartrate, 10)
+                    count++
+                }
+            });
+            var AvgRate = Trate/count;
+            if(this.props.type==="Swimming"){
+                AvgRate = AvgRate + 20;
+            }
+            if(this.props.type==="Cycling"){
+                AvgRate = AvgRate + 30;  
+            }
+            distance = Tsteps/1.35;
             
-                console.log(this.props.name);              
-                console.log(user);
-                console.log(Tsteps);              
-                console.log(distance);              
-                console.log(calories);  
-                var Arr = this.state.user_data;
-                Arr.push({Tsteps,distance,calories,user}); 
-                var sArr = this.state.steps;
-                sArr.push(Tsteps); 
-                var dArr = this.state.distance;
-                dArr.push(distance); 
-                var cArr = this.state.calories;
-                cArr.push(calories); 
-                this.setState({
-                    data:response[0],
-                    user_data: Arr,
-                    steps: sArr,
-                    distance: dArr,
-                    calories: cArr
-                })  
-                console.log(this.state);   
-            })                                          
-        }) 
-        this.setState({
-            SchartData:{
-                labels: this.props.users,
-                datasets:[
-                  {
-                    label:'Total Steps',
-                    data:this.state.steps,
-                    backgroundColor:'rgba(255, 99, 132, 0.6)',
-                    pointRadius: 0
-                  },
-                  {
-                    label:'Total Distance',
-                    data:this.state.distance,
-                    backgroundColor:'rgba(255, 0, 132, 0.6)',
-                    pointRadius: 0
-                  },
-                  {
-                    label:'Total Calories',
-                    data:this.state.calories,
-                    backgroundColor:'rgba(255, 120, 132, 0.6)',
-                    pointRadius: 0
-                  }
-                ],
-                
-            },
-            DchartData:{
-                labels: this.props.users,
-                datasets:[
-                  {
-                    label:'Total Steps',
-                    data:this.state.distance,
-                    backgroundColor:'rgba(255, 99, 132, 0.6)',
-                    pointRadius: 0
-                  }
-                ]
-            },
-            CchartData:{
-                labels: this.props.users,
-                datasets:[
-                  {
-                    label:'Total Steps',
-                    data:this.state.calories,
-                    backgroundColor:'rgba(255, 99, 132, 0.6)',
-                    pointRadius: 0
-                  }
-                ]
-            },
-              
-        })           
+            calories = distance/18.8;                        
+            if(this.props.type==="Swimming"){
+                calories = calories * 8;
+            }
+            if(this.props.type==="Cycling"){
+                calories = calories * 5;  
+            }
+            this.setState({
+                data:response[0],
+                user_data: {AvgRate,Tsteps,distance,calories},
+                user:response[0].user
+            })   
+
+        })          
     }
-    handleClose(){
-		this.setState({show:false})
-	}
-	handleShow(){
-		this.setState({show:true})
-	}
   render(){
     return(
-      <Card>
+      <>
         <br/>
-        <Button onClick={this.handleShow}>{this.props.name}</Button>
-        {this.state.user_data.map(user => {
-            
-            if (user.user === currentUser){
-                return(
-                    <div key ={user.user}>
-                        <div>
-                            {user.Tsteps}
-                        </div>
-                        <div>
-                            {user.distance}
-                        </div>
-                        <div>
-                            {user.calories}
-                        </div>
-                    </div>
-                )
-            }                                     
-        })}
+            <Row >
+                <Col>
+                    <div> Heart Rate </div>
+                    <div>{this.state.user_data.AvgRate}</div>
+                </Col>
+                <Col>
+                    <div> Total Distance </div>
+                    <div>{this.state.user_data.distance}</div>
+                </Col>
+                <Col>
+                    <div> Total calories burned </div>
+                    <div>{this.state.user_data.calories}</div>
+                </Col>
+            </Row>
+                
         <br/>
-        <Modal show={this.state.show} onHide={this.handleClose} animation={false}>
-            <Modal.Header closeButton>
-            <Modal.Title>Event details</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-				<Chart chartData={this.state.SchartData} title="Event Summery"></Chart>
-		    </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={this.handleClose}>
-                Close
-              </Button>
-            </Modal.Footer>
-      	</Modal>
-      </Card>            
+      </>            
     )
   }
     
